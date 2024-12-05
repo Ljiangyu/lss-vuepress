d35861a0f7ced123546f31751d637fd89ca87aac
Ljiangyu <104154611+Ljiangyu@users.noreply.github.com>
2024-12-05 15:27:55 +0800

    git comment 纠正8

--- build/delComponents.js
+++ build/delComponents.js
@@ -1,6 +1,6 @@
 // delComponents.js
 import * as fs from 'fs';
-import { findMarkdown } from './findMarkdown.js';
+import findMarkdown from './findMarkdown.js';
 const rootDir = "./docs";
 
 findMarkdown(rootDir, delComponents);
